86ea4e7ec044f6ad13671f3857c370e5bf804e8c
jotapeastorga <jotapeastorga@gmail.com>
2020-10-11 23:58:55 -0300

    cronometro

---
+++ js/Cronometro.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    var tiempo = {
+                    hora: 0,
+                    minuto: 0,
+                    segundo: 0,
+                    centesima:0,
+                    corriendo : 0
+    };
+    $("button").click(function(){
+        if ( $(this).text() == 'Arrancar' ){
+            $(this).text('Parar');                        
+            tiempo.corriendo = setInterval(function(){
+                // centesimas
+                tiempo.centesima++;
+                if(tiempo.centesima >= 100){
+                    tiempo.centesima =0;
+                    tiempo.segundo++;
+                }  
+                // Segundos
+                if(tiempo.segundo >= 60){
+                    tiempo.segundo = 0;
+                    tiempo.minuto++;
+                }      
+                // Minutos
+                if(tiempo.minuto >= 60){
+                    tiempo.minuto = 0;
+                    tiempo.hora++;
+                }
+                $("#hora").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
+                $("#minuto").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
+                $("#segundo").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
+                $("#centesima").text(tiempo.centesima < 10 ? '0' + tiempo.centesima : tiempo.centesima);
+            }, 10);
+        }
+        else 
+        {
+            $(this).text('Stop');
+            clearInterval(tiempo.corriendo);
+        }
+    })          
+});
