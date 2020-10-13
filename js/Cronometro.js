eb1060db730811fadd0489963c1cc3157677ee65
jotapeastorga <jotapeastorga@gmail.com>
2020-10-13 16:37:34 -0300

    fix time

--- js/Cronometro.js
+++ js/Cronometro.js
@@ -1,13 +1,11 @@
+let tiempo = {
+    minuto: 0,
+    segundo: 0,
+    centesima: 0,
+    corriendo: 0
+};
 
-function tiempoCrono(estado) {
-
-    var tiempo = {
-        hora: 0,
-        minuto: 0,
-        segundo: 0,
-        centesima: 0,
-        corriendo: 0
-    };
+function comenzar() {
 
 
     tiempo.corriendo = setInterval(function () {
@@ -25,20 +23,30 @@ function tiempoCrono(estado) {
         // Minutos
         if (tiempo.minuto >= 60) {
             tiempo.minuto = 0;
-            tiempo.hora++;
         }
-        $("#hora").text(tiempo.hora < 10 ? '0' + tiempo.hora : tiempo.hora);
         $("#minuto").text(tiempo.minuto < 10 ? '0' + tiempo.minuto : tiempo.minuto);
         $("#segundo").text(tiempo.segundo < 10 ? '0' + tiempo.segundo : tiempo.segundo);
         $("#centesima").text(tiempo.centesima < 10 ? '0' + tiempo.centesima : tiempo.centesima);
     }, 10);
 
-    if (estado === 'reseteo') {
-        clearInterval(tiempo.corriendo);
-        alert('Perdiste :(');
-    }
+}
 
+function reiniciar() {
+    clearInterval(tiempo.corriendo);
+    console.log(tiempo);
+    console.log('al borrar' + tiempo);
+    tiempo.centesima = 0;
+    tiempo.corriendo = 0;
+    tiempo.segundo = 0;
+    $("#minuto").text('00');
+    $("#segundo").text('00');
+    $("#centesima").text('00');
+    alert('Perdiste :(');
 
 
 }
+
+
+
+
 
